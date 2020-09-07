ab484d7c9e69f354f97c9dbc9141ec43adeba236
Ben Vandervalk <ben.vvalk@gmail.com>
2020-09-07 09:39:09 -0400

    benv: rewrite NUM layer and map to inner right thumb key

--- keyboards/ergodox_ez/keymaps/benv-steno/keymap.c
+++ keyboards/ergodox_ez/keymaps/benv-steno/keymap.c
@@ -5,7 +5,7 @@
 #include "virtser.h"
 
 #define BASE 0    // default layer (Dvorak)
-#define SYMB 1    // numbers/symbols layer
+#define NUM 1     // numbers layer
 #define TXBOLT 2  // TxBolt Steno Virtual Serial
 #define STENONAV 3 // quick access to arrow keys from TXBOLT layer
 #define NAV 4     // navigation (arrows, page up/down)
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      |      |       |      |        |      |
  *                                 | Esc  | Tab  |------|       |------| Enter  |Space |
- *                                 |(Shft)|      | BOLT |       |      |        |(Shft)|
+ *                                 |(Shft)|      | BOLT |       |      | (NUM)  |(Shft)|
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -53,49 +53,49 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            KC_NO,     KC_NO,         KC_NO,         KC_NO,          KC_NO,
   KC_NO, KC_NO,
   KC_NO,
-  KC_NO, KC_ENT, RSFT_T(KC_SPC),
+  KC_NO, LT(NUM, KC_ENT), RSFT_T(KC_SPC),
 ),
-/* Keymap 1: Symbol Layer
+/* NUM layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |  ~/`   |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |           |  F7  |  F8  |  F9  | F10  | F11  | F12  |        |
+ * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |   !  |   @  |   {  |   }  |   |  |      |           |      |   Up |   7  |   8  |   9  | //?  |  \/|   |
+ * |        |      |   7  |  8   |  9   |      |      |           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   #  |   $  |   (  |   )  |   `  |------|           |------| Down |   4  |   5  |   6  | -/_  |  =/+   |
+ * |        |      |   4  |  5   |  6   |  -   |------|           |------|      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |        |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   &  |   1  |   2  |   3  |   \  |        |
+ * |        |      |   1  |  2   |  3   |  =   |      |           |      |      |      |      |      |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       |   0  |    . |      |   =  |      |
+ *   |      |      |      |      |      |                                       |      |      |      |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
  *                                 |      |      |      |       |      |      |      |
- *                                 |      |      |------|       |------|      |      |
+ *                                 |   0  |      |------|       |------|      |      |
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
 // SYMBOLS
-[SYMB] = LAYOUT_ergodox(
-       // left hand
-       KC_GRV, KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,
-       KC_TRNS,KC_EXLM,KC_AT,  KC_LCBR,KC_RCBR,KC_PIPE,KC_TRNS,
-       KC_TRNS,KC_HASH,KC_DLR, KC_LPRN,KC_RPRN,KC_GRV,
-       KC_TRNS,KC_PERC,KC_CIRC,KC_LBRC,KC_RBRC,KC_TILD,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-                                       KC_TRNS,KC_TRNS,
-                                               KC_TRNS,
-                               KC_TRNS,KC_TRNS,KC_TRNS,
-       // right hand
-       KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,    KC_F12,   KC_TRNS,
-       KC_TRNS, KC_UP,   KC_7,     KC_8,     KC_9,      KC_ASTR,  KC_LBRC,
-                KC_DOWN, KC_4,     KC_5,     KC_6,      KC_MINS,  KC_EQL,
-       KC_TRNS, KC_AMPR, KC_1,     KC_2,     KC_3,      KC_BSLS,  KC_TRNS,
-                         KC_0,     KC_DOT,   KC_TRNS,   KC_EQL,   KC_TRNS,
-       KC_TRNS, KC_TRNS,
-       KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS
+[NUM] = LAYOUT_ergodox(
+  // left hand
+  KC_TRNS,   KC_TRNS,    KC_TRNS,     KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,
+  KC_TRNS,   KC_TRNS,    KC_7,        KC_8,       KC_9,      KC_TRNS,   KC_TRNS,
+  KC_TRNS,   KC_TRNS,    KC_4,        KC_5,       KC_6,      KC_TRNS,
+  KC_TRNS,   KC_TRNS,    KC_1,        KC_2,       KC_3,      KC_TRNS,   KC_TRNS,
+  KC_TRNS,   KC_TRNS,    KC_TRNS,     KC_TRNS,    KC_TRNS,
+                                                                        KC_TRNS,  KC_TRNS,
+                                                                                  KC_TRNS,
+                                                                 KC_0,  KC_TRNS,  KC_TRNS,
+  // right hand
+  KC_TRNS,   KC_TRNS,    KC_TRNS,     KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,   
+  KC_TRNS,   KC_TRNS,    KC_TRNS,     KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,
+             KC_TRNS,    KC_TRNS,     KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,
+  KC_TRNS,   KC_TRNS,    KC_TRNS,     KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,
+                         KC_TRNS,     KC_TRNS,    KC_TRNS,   KC_TRNS,   KC_TRNS,
+  KC_TRNS, KC_TRNS,
+  KC_TRNS,
+  KC_TRNS, KC_TRNS, KC_TRNS
 ),
 // TxBolt Codes
 #define Sl 0b00000001
@@ -278,7 +278,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,      KC_Q,      KC_W,      KC_E,      KC_R,       KC_T,  KC_LPRN,
   KC_CLCK,     KC_A,      KC_S,      KC_D,      KC_F,       KC_G,
   KC_LSFT,     KC_Z,      KC_X,      KC_C,      KC_V,       KC_B,  KC_MINS,
-  KC_LCTL,     KC_LWIN,   KC_LALT,   KC_LBRC,   MO(SYMB),
+  KC_LCTL,     KC_LWIN,   KC_LALT,   KC_LBRC,   MO(NUM),
                                                              MO(NAV), TG(TXBOLT),
                                                                            KC_NO,
                                                         KC_SPC, KC_LCTL, KC_LALT,
@@ -287,7 +287,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_RPRN,  KC_Y,      KC_U,      KC_I,      KC_O,       KC_P,   KC_BSPC,
             KC_H,      KC_J,      KC_K,      KC_L,    KC_SCLN,   KC_ENT,
    KC_EQL,  KC_N,      KC_M,   KC_COMM,    KC_DOT,    KC_SLSH,   KC_RSFT,
-        MO(SYMB),   KC_BSLS,   KC_RALT,   KC_RWIN,    KC_RCTL,
+         MO(NUM),   KC_BSLS,   KC_RALT,   KC_RWIN,    KC_RCTL,
   TG(TXBOLT), MO(NAV),
   KC_NO,
   KC_RALT, KC_RCTL, KC_TAB
@@ -295,7 +295,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_TAP_TOGGLE(SYMB)                // FN1 - Momentary Layer 1 (Symbols)
+    [1] = ACTION_LAYER_TAP_TOGGLE(NUM)                // FN1 - Momentary Layer 1 (Symbols)
 };
 
 uint8_t chord[4] = {0,0,0,0};
