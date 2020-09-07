230f654fc799ab65718cffec4c4c216342ef8ba6
Ben Vandervalk <ben.vvalk@gmail.com>
2020-09-07 11:03:54 -0400

    benv: fix compile error due to trailing comma [buildfix]

--- keyboards/ergodox_ez/keymaps/benv-steno/keymap.c
+++ keyboards/ergodox_ez/keymaps/benv-steno/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            KC_NO,     KC_NO,         KC_NO,         KC_NO,          KC_NO,
   KC_NO, KC_NO,
   KC_NO,
-  KC_NO, LT(NUM, KC_ENT), RSFT_T(KC_SPC),
+  KC_NO, LT(NUM, KC_ENT), RSFT_T(KC_SPC)
 ),
 /* Keymap 1: NUM layer
  *
