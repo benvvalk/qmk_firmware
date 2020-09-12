0c04db44b1abd9fb5480c23acab7185ca24a4be4
Ben Vandervalk <ben.vvalk@gmail.com>
2020-09-12 11:37:12 -0400

    benv: map LED1 to TXBOLT layer

--- keyboards/ergodox_ez/keymaps/benv-steno/keymap.c
+++ keyboards/ergodox_ez/keymaps/benv-steno/keymap.c
@@ -482,15 +482,10 @@ void matrix_scan_user(void) {
     ergodox_right_led_2_off();
     ergodox_right_led_3_off();
     switch (layer) {
-      // TODO: Make this relevant to the ErgoDox EZ.
-        case 1:
+        case TXBOLT:
             ergodox_right_led_1_on();
             break;
-        case 2:
-            ergodox_right_led_2_on();
-            break;
         default:
-            // none
             break;
     }
 
