68e2ac3c837f2fbef9c80018c217716c41056d90
Ben Vandervalk <ben.vvalk@gmail.com>
2020-09-27 15:53:34 -0400

    benv-steno/rules.mk: add some recommended settings for steno [cleanup]

--- keyboards/ergodox_ez/keymaps/benv-steno/rules.mk
+++ keyboards/ergodox_ez/keymaps/benv-steno/rules.mk
@@ -1,3 +1,7 @@
+# See https://paulfioravanti.com/blog/starting-stenography-with-an-ergodox/
+
+STENO_ENABLE = yes
+FORCE_NKRO = yes
 VIRTSER_ENABLE = yes
 # Not enough interupts, so something has to go
 MOUSEKEY_ENABLE = no
