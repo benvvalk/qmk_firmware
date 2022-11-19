03c8fb87b1880b4fae1d734511c26bdcc4f82598
Ben Vandervalk <ben.vvalk@gmail.com>
2022-11-19 15:57:09 -0500

    _MAIN: set upper right key to slash, move backspace to thumb

--- keyboards/boardsource/4x12/keymaps/benv/keymap.c
+++ keyboards/boardsource/4x12/keymaps/benv/keymap.c
@@ -13,10 +13,10 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_MAIN] = LAYOUT_ortho_4x12(
-    KC_GRV,                KC_QUOT,      KC_COMM,      KC_DOT,       KC_P,                KC_Y,  KC_F,  KC_G,         KC_C,         KC_R,         KC_L,         KC_BSPC,
-    LT(_BRACKET, KC_ESC),  LSFT_T(KC_A), LCTL_T(KC_O), LGUI_T(KC_E), LALT_T(KC_U),        KC_I,  KC_D,  RALT_T(KC_H), RGUI_T(KC_T), RCTL_T(KC_N), RSFT_T(KC_S), KC_ENT,
-    TO(_MAIN),             KC_SCLN,      KC_Q,         KC_J,         KC_K,                KC_X,  KC_B,  KC_M,         KC_W,         KC_V,         KC_Z,         TO(_STENO),
-    KC_PIPE,               KC_LCTL,      KC_LALT,      MO(_NUM),     LT(_ARROWS, KC_TAB), KC_NO, KC_NO, KC_SPC,       KC_LEFT,      KC_DOWN,      KC_UP,        KC_RGHT
+    KC_GRV,                KC_QUOT,      KC_COMM,      KC_DOT,       KC_P,                KC_Y,  KC_F,    KC_G,         KC_C,         KC_R,         KC_L,         KC_SLSH,
+    LT(_BRACKET, KC_ESC),  LSFT_T(KC_A), LCTL_T(KC_O), LGUI_T(KC_E), LALT_T(KC_U),        KC_I,  KC_D,    RALT_T(KC_H), RGUI_T(KC_T), RCTL_T(KC_N), RSFT_T(KC_S), KC_ENT,
+    TO(_MAIN),             KC_SCLN,      KC_Q,         KC_J,         KC_K,                KC_X,  KC_B,    KC_M,         KC_W,         KC_V,         KC_Z,         TO(_STENO),
+    KC_PIPE,               KC_LCTL,      KC_LALT,      MO(_NUM),     LT(_ARROWS, KC_TAB), KC_NO, KC_BSPC, KC_SPC,       KC_LEFT,      KC_DOWN,      KC_UP,        KC_RGHT
   ),
   [_NUM] = LAYOUT_ortho_4x12(
     _______, _______, _______, _______, _______, _______, _______, KC_7, KC_8,   KC_9,    _______, _______,
