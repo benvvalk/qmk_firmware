939043cbc4f3302c95a5cf02b6fcfd89ccb3a6c0
Ben Vandervalk <ben.vvalk@gmail.com>
2022-11-19 14:08:42 -0500

    replace layer _RAISE -> _NUM

--- keyboards/boardsource/4x12/keymaps/benv/keymap.c
+++ keyboards/boardsource/4x12/keymaps/benv/keymap.c
@@ -4,22 +4,22 @@
 
 enum layers {
     _MAIN,
-    _RAISE,
+    _NUM,
     _ARROWS,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_MAIN] = LAYOUT_ortho_4x12(
-    KC_TAB,  KC_QUOT,      KC_COMM,      KC_DOT,       KC_P,                KC_Y,  KC_F,  KC_G,               KC_C,         KC_R,         KC_L,         KC_BSPC,
-    KC_ESC,  LSFT_T(KC_A), LCTL_T(KC_O), LGUI_T(KC_E), LALT_T(KC_U),        KC_I,  KC_D,  RALT_T(KC_H),       RGUI_T(KC_T), RCTL_T(KC_N), RSFT_T(KC_S), KC_QUOT,
-    KC_LSFT, KC_SCLN,      KC_Q,         KC_J,         KC_K,                KC_X,  KC_B,  KC_M,               KC_W,         KC_V,         KC_Z,         KC_ENT,
-    KC_PIPE, KC_LCTL,      KC_LALT,      KC_LGUI,      LT(_ARROWS, KC_TAB), KC_NO, KC_NO, LT(_RAISE, KC_SPC), KC_LEFT,      KC_DOWN,      KC_UP,        KC_RGHT
+    KC_TAB,  KC_QUOT,      KC_COMM,      KC_DOT,       KC_P,                KC_Y,  KC_F,  KC_G,         KC_C,         KC_R,         KC_L,         KC_BSPC,
+    KC_ESC,  LSFT_T(KC_A), LCTL_T(KC_O), LGUI_T(KC_E), LALT_T(KC_U),        KC_I,  KC_D,  RALT_T(KC_H), RGUI_T(KC_T), RCTL_T(KC_N), RSFT_T(KC_S), KC_QUOT,
+    KC_LSFT, KC_SCLN,      KC_Q,         KC_J,         KC_K,                KC_X,  KC_B,  KC_M,         KC_W,         KC_V,         KC_Z,         KC_ENT,
+    KC_PIPE, KC_LCTL,      KC_LALT,      MO(_NUM),     LT(_ARROWS, KC_TAB), KC_NO, KC_NO, KC_SPC,       KC_LEFT,      KC_DOWN,      KC_UP,        KC_RGHT
   ),
-  [_RAISE] = LAYOUT_ortho_4x12(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  S(KC_NUHS), S(KC_NUBS), KC_HOME, KC_END,  _______,
-    QK_BOOT,   _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+  [_NUM] = LAYOUT_ortho_4x12(
+    _______, _______, _______, _______, _______, _______, _______, KC_7, KC_8,   KC_9,    _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, KC_4, KC_5,   KC_6,    _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, KC_1, KC_2,   KC_3,    _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, KC_0, KC_DOT, _______, _______, _______
   ),
   [_ARROWS] = LAYOUT_ortho_4x12(
     _______, _______, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, _______, _______,
