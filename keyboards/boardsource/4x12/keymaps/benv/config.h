179292fd7c41ef3dd99d2480d15445b357fb3a57
Ben Vandervalk <ben.vvalk@gmail.com>
2022-11-19 12:57:39 -0500

    _MAIN: add mod-taps for shift/ctrl/gui/alt

---
+++ keyboards/boardsource/4x12/keymaps/benv/config.h
@@ -0,0 +1,14 @@
+// These variables relate to the behaviour of the mod-tap keys
+// for shift/ctrl/gui/alt on the home row.
+//
+// There is some excellent documentation about these variables at:
+// https://github.com/qmk/qmk_firmware/blob/master/docs/tap_hold.md
+
+#undef PERMISSIVE_HOLD
+#define PERMISSIVE_HOLD
+
+#undef IGNORE_MOD_TAP_INTERRUPT
+#define IGNORE_MOD_TAP_INTERRUPT
+
+#undef RETRO_TAPPING
+#define RETRO_TAPPING
