b6fe20ce80c4c2f2d9f825da1796638c394a9234
Ben Vandervalk <ben.vvalk@gmail.com>
2022-11-19 20:46:52 -0500

    _MAIN: move backspace back to top right corner
    ... and move forward slash to top right corner on _ARROWS layer.

--- keyboards/boardsource/4x12/keymaps/benv/keymap.c
+++ keyboards/boardsource/4x12/keymaps/benv/keymap.c
@@ -13,7 +13,7 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_MAIN] = LAYOUT_ortho_4x12(
-    KC_GRV,                KC_QUOT,      KC_COMM,      KC_DOT,       KC_P,                KC_Y,     KC_F,    KC_G,         KC_C,         KC_R,         KC_L,         KC_SLSH,
+    KC_GRV,                KC_QUOT,      KC_COMM,      KC_DOT,       KC_P,                KC_Y,     KC_F,    KC_G,         KC_C,         KC_R,         KC_L,         KC_BSPC,
     LT(_BRACKET, KC_ESC),  LSFT_T(KC_A), LCTL_T(KC_O), LGUI_T(KC_E), LALT_T(KC_U),        KC_I,     KC_D,    RALT_T(KC_H), RGUI_T(KC_T), RCTL_T(KC_N), RSFT_T(KC_S), KC_ENT,
     TO(_MAIN),             KC_SCLN,      KC_Q,         KC_J,         KC_K,                KC_X,     KC_B,    KC_M,         KC_W,         KC_V,         KC_Z,         KC_APP,
     KC_PIPE,               KC_LCTL,      KC_LALT,      KC_NO,        LT(_ARROWS, KC_TAB), MO(_NUM), KC_BSPC, KC_SPC,       KC_LEFT,      KC_DOWN,      KC_UP,        KC_RGHT
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, KC_0, KC_DOT, _______, _______, _______
   ),
   [_ARROWS] = LAYOUT_ortho_4x12(
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, _______, KC_SLSH,
     _______, _______, _______, _______, _______, _______, KC_HOME, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_END,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
