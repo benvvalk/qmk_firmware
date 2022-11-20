a147956a2e42e0df07944033ae1577c426815c60
Ben Vandervalk <ben.vvalk@gmail.com>
2022-11-19 20:10:48 -0500

    rules.mk: add `STENO_PROTOCOL = geminipr` for clarity
    `geminipr` seems to be the default, but adding this line is a good
    reminder that my keymap.c code relies on being in Gemini mode.

--- keyboards/boardsource/4x12/keymaps/benv/rules.mk
+++ keyboards/boardsource/4x12/keymaps/benv/rules.mk
@@ -1,6 +1,7 @@
 # See https://paulfioravanti.com/blog/starting-stenography-with-an-ergodox/
 
 STENO_ENABLE = yes
+STENO_PROTOCOL = geminipr
 FORCE_NKRO = yes
 VIRTSER_ENABLE = yes
 # Not enough interupts, so something has to go
