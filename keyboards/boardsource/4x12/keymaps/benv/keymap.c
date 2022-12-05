60a5298f93f4f5dd1a11fcf3fbd73db566ea3d31
Ben Vandervalk <ben.vvalk@gmail.com>
2022-12-05 09:02:09 -0500

    _MAIN: move Shift mod taps back to pinky home row
    And put the displaced layer taps for the _BRACKET and _SYM layers on
    the index finger top row.

--- keyboards/boardsource/4x12/keymaps/benv/keymap.c
+++ keyboards/boardsource/4x12/keymaps/benv/keymap.c
@@ -16,10 +16,10 @@ enum layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_MAIN] = LAYOUT_ortho_4x12(
-    KC_GRV,            KC_QUOT,            KC_COMM,      KC_DOT,            KC_P,                 KC_Y,     KC_F,     KC_G,             KC_C,         KC_R,         KC_L,           KC_SLSH,
-    LT(_MODE, KC_ESC), LT(_BRACKET, KC_A), LCTL_T(KC_O), LGUI_T(KC_E),      LALT_T(KC_U),         KC_I,     KC_D,     RALT_T(KC_H),     RGUI_T(KC_T), RCTL_T(KC_N), LT(_SYM, KC_S), KC_ENT,
-    KC_LSFT,           LSFT_T(KC_SCLN),    KC_Q,         KC_J,              KC_K,                 KC_X,     KC_B,     KC_M,             KC_W,         KC_V,         RSFT_T(KC_Z),   KC_RSFT,
-    KC_PIPE,           KC_LCTL,            KC_LALT,      LT(_FKEY, KC_DEL), LT(_ARROWS, KC_BSPC), KC_LSFT,  KC_RSFT,  LT(_NUM, KC_SPC), KC_TAB,       XXXXXXX,      XXXXXXX,        XXXXXXX
+    KC_GRV,            KC_QUOT,      KC_COMM,      LT(_BRACKET, KC_DOT), KC_P,                 KC_Y,     KC_F,     KC_G,             LT(_SYM, KC_C), KC_R,         KC_L,         KC_SLSH,
+    LT(_MODE, KC_ESC), LSFT_T(KC_A), LCTL_T(KC_O), LGUI_T(KC_E),         LALT_T(KC_U),         KC_I,     KC_D,     RALT_T(KC_H),     RGUI_T(KC_T),   RCTL_T(KC_N), RSFT_T(KC_S), KC_ENT,
+    KC_LSFT,           KC_SCLN,      KC_Q,         KC_J,                 KC_K,                 KC_X,     KC_B,     KC_M,             KC_W,           KC_V,         KC_Z,         KC_RSFT,
+    KC_PIPE,           KC_LCTL,      KC_LALT,      LT(_FKEY, KC_DEL),    LT(_ARROWS, KC_BSPC), KC_LSFT,  KC_RSFT,  LT(_NUM, KC_SPC), KC_TAB,         XXXXXXX,      XXXXXXX,      XXXXXXX
   ),
   [_NUM] = LAYOUT_ortho_4x12(
     _______, _______, KC_1,    KC_2,   KC_3, _______,  _______, _______, _______, _______, _______, _______,
