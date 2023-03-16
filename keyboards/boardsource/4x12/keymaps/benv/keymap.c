d0f236aecbcc27f6cde5c1d882e149fdffd1185a
Ben Vandervalk <ben.vvalk@gmail.com>
2023-03-16 09:40:12 -0400

    _STENO: add # keys to lower row index finger
    I think these could be more comfortable than the outer thumb
    keys that I'm currently using. As per usual, I will keep
    both locations and see what sticks.

--- keyboards/boardsource/4x12/keymaps/benv/keymap.c
+++ keyboards/boardsource/4x12/keymaps/benv/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_STENO] = LAYOUT_ortho_4x12(
     STN_N1,    STN_S1,  STN_TL,  STN_PL,  STN_HL,  STN_ST1, STN_ST3, STN_FR,  STN_PR,  STN_LR,  STN_TR,  STN_DR,
     PB_3,      STN_S2,  STN_KL,  STN_WL,  STN_RL,  STN_ST2, STN_ST4, STN_RR,  STN_BR,  STN_GR,  STN_SR,  STN_ZR,
-    _______,   STN_N1,  STN_ST1, _______, _______, _______, _______, _______, _______, STN_ST2, STN_N2,  _______,
+    _______,   STN_N1,  STN_ST1, _______, STN_N1,  STN_ST2, STN_ST4, STN_N2,  _______, STN_ST2, STN_N2,  _______,
     _______,   _______, _______, STN_A,   STN_O,   STN_N1,  STN_N2,  STN_E,   STN_U,   _______, _______, _______
   ),
   [_MODE] = LAYOUT_ortho_4x12(
